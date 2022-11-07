d22d5dbd8d89ecc018580ef88ce4c8ad000fc914
Martin Neuner <mfritzn@gmail.com>
2022-11-07 10:25:46 +0000

    Add Binding Precedence Tests

--- tests/BoolImplTest.elm
+++ tests/BoolImplTest.elm
@@ -17,7 +17,7 @@ suite =
                     case (run BoolImpl.formula_p "a & b & c") of
                         Ok formula -> equals formula expectedFormula
                             |> Expect.equal Basics.True
-                            |> Expect.onFail ("Expected Formula " ++ toString expectedFormula ++ " but got " ++ toString formula ++ "instead")
+                            |> Expect.onFail ("Expected Formula " ++ toString expectedFormula ++ " but got " ++ toString formula ++ " instead")
                         Err _ -> Expect.fail  "expected the parser to succeed"
                 , test "Or" <|
                 \_ ->
@@ -26,7 +26,7 @@ suite =
                     case (run BoolImpl.formula_p "a | b | c") of
                         Ok formula -> equals formula expectedFormula
                             |> Expect.equal Basics.True
-                            |> Expect.onFail ("Expected Formula " ++ toString expectedFormula ++ " but got " ++ toString formula ++ "instead")
+                            |> Expect.onFail ("Expected Formula " ++ toString expectedFormula ++ " but got " ++ toString formula ++ " instead")
                         Err _ -> Expect.fail  "expected the parser to succeed"
                 , test "Impl" <|
                 \_ ->
@@ -35,8 +35,100 @@ suite =
                     case (run BoolImpl.formula_p "a -> b -> c") of
                         Ok formula -> equals formula expectedFormula
                             |> Expect.equal Basics.True
-                            |> Expect.onFail ("Expected Formula " ++ toString expectedFormula ++ " but got " ++ toString formula ++ "instead")
+                            |> Expect.onFail ("Expected Formula " ++ toString expectedFormula ++ " but got " ++ toString formula ++ " instead")
                         Err _ -> Expect.fail  "expected the parser to succeed"
                 ]
             ]
+            ,describe "Binding Precedence"
+                [test " ~ > & 1" <|
+                \_ ->
+                    let expectedFormula = And (Neg (Var "a")) (Var "b")
+                    in
+                    case (run BoolImpl.formula_p "~a & b") of
+                        Ok formula -> equals formula expectedFormula
+                            |> Expect.equal Basics.True
+                            |> Expect.onFail ("Expected Formula " ++ toString expectedFormula ++ " but got " ++ toString formula ++ " instead")
+                        Err _ -> Expect.fail  "expected the parser to succeed"
+                , test " ~ > & 2" <|
+                \_ ->
+                    let expectedFormula = And (Var "a") (Neg(Var "b"))
+                    in
+                    case (run BoolImpl.formula_p "a & ~b") of
+                        Ok formula -> equals formula expectedFormula
+                            |> Expect.equal Basics.True
+                            |> Expect.onFail ("Expected Formula " ++ toString expectedFormula ++ " but got " ++ toString formula ++ " instead")
+                        Err _ -> Expect.fail  "expected the parser to succeed"
+                , test " ~ > | 1" <|
+                \_ ->
+                    let expectedFormula = Or (Neg (Var "a")) (Var "b")
+                    in
+                    case (run BoolImpl.formula_p "~a | b") of
+                        Ok formula -> equals formula expectedFormula
+                            |> Expect.equal Basics.True
+                            |> Expect.onFail ("Expected Formula " ++ toString expectedFormula ++ " but got " ++ toString formula ++ " instead")
+                        Err _ -> Expect.fail  "expected the parser to succeed"
+                , test "~ > | 2" <|
+                \_ ->
+                    let expectedFormula = Or (Var "a") (Neg(Var "b"))
+                    in
+                    case (run BoolImpl.formula_p "a | ~b") of
+                        Ok formula -> equals formula expectedFormula
+                            |> Expect.equal Basics.True
+                            |> Expect.onFail ("Expected Formula " ++ toString expectedFormula ++ " but got " ++ toString formula ++ " instead")
+                        Err _ -> Expect.fail  "expected the parser to succeed"
+                , test " ~ > -> 1" <|
+                \_ ->
+                    let expectedFormula = Impl (Neg (Var "a")) (Var "b")
+                    in
+                    case (run BoolImpl.formula_p "~a -> b") of
+                        Ok formula -> equals formula expectedFormula
+                            |> Expect.equal Basics.True
+                            |> Expect.onFail ("Expected Formula " ++ toString expectedFormula ++ " but got " ++ toString formula ++ " instead")
+                        Err _ -> Expect.fail  "expected the parser to succeed"
+                , test "~ > ->  2" <|
+                \_ ->
+                    let expectedFormula = Impl (Var "a") (Neg(Var "b"))
+                    in
+                    case (run BoolImpl.formula_p "a -> ~b") of
+                        Ok formula -> equals formula expectedFormula
+                            |> Expect.equal Basics.True
+                            |> Expect.onFail ("Expected Formula " ++ toString expectedFormula ++ " but got " ++ toString formula ++ " instead")
+                        Err _ -> Expect.fail  "expected the parser to succeed"
+                , test " & > -> 1" <|
+                \_ ->
+                    let expectedFormula = Impl (And (Var "a") (Var "b")) (Var "c")
+                    in
+                    case (run BoolImpl.formula_p "a & b -> c") of
+                        Ok formula -> equals formula expectedFormula
+                            |> Expect.equal Basics.True
+                            |> Expect.onFail ("Expected Formula " ++ toString expectedFormula ++ " but got " ++ toString formula ++ " instead")
+                        Err _ -> Expect.fail  "expected the parser to succeed"
+                , test "& > ->  2" <|
+                \_ ->
+                    let expectedFormula = Impl (Var "a") (And (Var "b") (Var "c"))
+                    in
+                    case (run BoolImpl.formula_p "a -> b & c") of
+                        Ok formula -> equals formula expectedFormula
+                            |> Expect.equal Basics.True
+                            |> Expect.onFail ("Expected Formula " ++ toString expectedFormula ++ " but got " ++ toString formula ++ " instead")
+                        Err _ -> Expect.fail  "expected the parser to succeed"
+                , test " | > -> 1" <|
+                \_ ->
+                    let expectedFormula = Impl (Or (Var "a") (Var "b")) (Var "c")
+                    in
+                    case (run BoolImpl.formula_p "a | b -> c") of
+                        Ok formula -> equals formula expectedFormula
+                            |> Expect.equal Basics.True
+                            |> Expect.onFail ("Expected Formula " ++ toString expectedFormula ++ " but got " ++ toString formula ++ " instead")
+                        Err _ -> Expect.fail  "expected the parser to succeed"
+                , test "| > ->  2" <|
+                \_ ->
+                    let expectedFormula = Impl (Var "a") (Or (Var "b") (Var "c"))
+                    in
+                    case (run BoolImpl.formula_p "a -> b | c") of
+                        Ok formula -> equals formula expectedFormula
+                            |> Expect.equal Basics.True
+                            |> Expect.onFail ("Expected Formula " ++ toString expectedFormula ++ " but got " ++ toString formula ++ " instead")
+                        Err _ -> Expect.fail  "expected the parser to succeed"
+                ]
         ]
