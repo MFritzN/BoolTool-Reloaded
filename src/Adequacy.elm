b1f02843e7379dc3b0dd3dc0e012ac224178ffc0
Martin Neuner <mfritzn@gmail.com>
2022-11-12 21:34:22 +0000

    Init isNotMonotone

--- src/Adequacy.elm
+++ src/Adequacy.elm
@@ -1,8 +1,10 @@
 module Adequacy exposing (..)
 
 import BoolImpl exposing (..)
-import Dict
+import Dict exposing (..)
+import Parser exposing (variable)
 import Set
+import Test.Runner.Failure exposing (Reason(..))
 
 
 existsAllInputNotEqInput : List Formula -> Basics.Bool -> Basics.Bool
@@ -17,3 +19,65 @@ existsAllInputNotEqInput list x =
                     Basics.False
         )
         list
+
+
+
+-- is montone
+
+
+exsistsIsNotMonotone : List Formula -> Basics.Bool
+exsistsIsNotMonotone list =
+    List.any isNotMontone list
+
+
+isNotMontone : Formula -> Basics.Bool
+isNotMontone formula =
+    let
+        variables =
+            Dict.fromList (List.map (\variable -> ( variable, Basics.True )) (Set.toList (getVariables formula)))
+    in
+    isMonotoneHelp formula variables (Dict.keys variables)
+
+
+isMonotoneHelp : Formula -> Dict String Bool -> List String -> Bool
+isMonotoneHelp formula variables remainingVariables =
+    case remainingVariables of
+        [] ->
+            case iterateVariables variables of
+                Nothing ->
+                    Basics.False
+
+                Just newVariables ->
+                    isMonotoneHelp formula newVariables (Dict.keys newVariables)
+
+        currentVar :: remainingVariablesTail ->
+            if not (Result.withDefault Basics.True (BoolImpl.evaluate formula (Dict.insert currentVar Basics.True variables))) && Result.withDefault Basics.True (BoolImpl.evaluate formula (Dict.insert currentVar Basics.True variables)) then
+                Basics.True
+
+            else
+                isMonotoneHelp formula variables remainingVariablesTail
+
+
+{-| Interprets the values in the dictonary as a binary number and increases it by 1 until all digits are 1.
+The function can be used to try every possible combination of variables. To do that start with a dict that only contains False as values.
+
+    iterateVariables {c: False, b: True, a: False} = {c: True, b: False, a: True}
+
+-}
+iterateVariables : Dict String Bool -> Maybe (Dict String Bool)
+iterateVariables dict =
+    iterateVariablesHelp [] (Dict.values dict)
+        |> Maybe.andThen (\a -> Just (Dict.fromList (List.map2 Tuple.pair (Dict.keys dict) a)))
+
+
+iterateVariablesHelp : List Basics.Bool -> List Basics.Bool -> Maybe (List Basics.Bool)
+iterateVariablesHelp changedVariables unchangedVariables =
+    case unchangedVariables of
+        [] ->
+            Nothing
+
+        Basics.False :: unchangedVariablesTail ->
+            Just (changedVariables ++ Basics.True :: unchangedVariablesTail)
+
+        Basics.True :: unchangedVariablesTail ->
+            iterateVariablesHelp (changedVariables ++ [ Basics.False ]) unchangedVariablesTail
--- tests/AdequacyTest.elm
+++ tests/AdequacyTest.elm
@@ -1,6 +1,6 @@
 module AdequacyTest exposing (..)
 
-import Adequacy exposing (existsAllInputNotEqInput)
+import Adequacy exposing (existsAllInputNotEqInput, exsistsIsNotMonotone)
 import BoolImpl exposing (..)
 import Expect exposing (..)
 import Test exposing (Test, describe, test)
@@ -15,6 +15,7 @@ firstSecondCondition =
             , testFirstSecondConditionHelp [ Neg (Var "x") ] Basics.False Basics.True
             , testFirstSecondConditionHelp [ And (Var "x") (Var "y") ] Basics.False Basics.False
             , testFirstSecondConditionHelp [ Or (Var "x") (Var "y") ] Basics.False Basics.False
+            , testFirstSecondConditionHelp [ Xor (Var "x") (Var "y") ] Basics.False Basics.False
             ]
         , describe "∃f ∈ X such that f (1,...,1) ≠ 1 "
             [ testFirstSecondConditionHelp [ BoolImpl.True ] Basics.True Basics.False
@@ -22,13 +23,35 @@ firstSecondCondition =
             , testFirstSecondConditionHelp [ Neg (Var "x") ] Basics.True Basics.True
             , testFirstSecondConditionHelp [ And (Var "x") (Var "y") ] Basics.True Basics.False
             , testFirstSecondConditionHelp [ Or (Var "x") (Var "y") ] Basics.True Basics.False
+            , testFirstSecondConditionHelp [ Xor (Var "x") (Var "y") ] Basics.True Basics.True
+            ]
+        , describe "∃f ∈ X which is not monotone "
+            [ monotonicityTestHelp [ BoolImpl.True ] Basics.False
+            , monotonicityTestHelp [ BoolImpl.False ] Basics.False
+            , monotonicityTestHelp [ Neg (Var "x") ] Basics.True
+            , monotonicityTestHelp [ And (Var "x") (Var "y") ] Basics.False
+            , monotonicityTestHelp [ Or (Var "x") (Var "y") ] Basics.False
+            , monotonicityTestHelp [ Xor (Var "x") (Var "y") ] Basics.True
             ]
         ]
 
 
 testFirstSecondConditionHelp : List Formula -> Basics.Bool -> Basics.Bool -> Test
 testFirstSecondConditionHelp testset allVariables expect =
-    test ("Testset: [" ++ String.slice 0 -2 (String.concat (List.map (\formula -> toString formula ++ ", ") testset)) ++ "]") <|
+    test (testSetToString testset) <|
         \_ ->
             existsAllInputNotEqInput testset allVariables
                 |> Expect.equal expect
+
+
+monotonicityTestHelp : List Formula -> Basics.Bool -> Test
+monotonicityTestHelp testset expect =
+    test (testSetToString testset) <|
+        \_ ->
+            exsistsIsNotMonotone testset
+                |> Expect.equal expect
+
+
+testSetToString : List Formula -> String
+testSetToString testset =
+    "Testset: [" ++ String.slice 0 -2 (String.concat (List.map (\formula -> toString formula ++ ", ") testset)) ++ "]"
