8cdafa82aad778cfee5c1d54b02112adccdcb742
Martin Neuner <mfritzn@gmail.com>
2022-12-04 09:59:54 +0000

    Add CNF, NNF, DNF

--- src/Representations.elm
+++ src/Representations.elm
@@ -74,6 +74,9 @@ view model =
                 Ok formula ->
                     [ renderTruthTable formula
                     , renderANF formula
+                    , renderNNF formula
+                    , renderCNF formula
+                    , renderDNF formula
                     ]
 
                 _ ->
@@ -378,3 +381,147 @@ calculateTruthTableHelp formula variables =
 
         Just newVariables ->
             ( Dict.values newVariables, evaluate formula newVariables ) :: calculateTruthTableHelp formula newVariables
+
+
+
+-- NNF
+
+
+renderNNF : Formula -> Html Msg
+renderNNF formula =
+    let
+        nnf =
+            calculateNNF formula
+    in
+    div [ class "box content" ]
+        [ h4 [] [ text "NNF" ]
+        , text (toString nnf)
+        ]
+
+
+implFree : Formula -> Formula
+implFree formula =
+    case formula of
+        Neg a ->
+            Neg (implFree a)
+
+        And a b ->
+            And (implFree a) (implFree b)
+
+        Or a b ->
+            Or (implFree a) (implFree b)
+
+        Impl a b ->
+            Or (Neg (implFree a)) (implFree b)
+
+        Xor a b ->
+            implFree (Or (And a b) (And (Neg a) (Neg b)))
+
+        a ->
+            a
+
+
+calculateNNF : Formula -> Formula
+calculateNNF formula =
+    case implFree formula of
+        Neg (Neg a) ->
+            calculateNNF a
+
+        Neg (And a b) ->
+            Or (calculateNNF (Neg a)) (calculateNNF (Neg b))
+
+        Neg (Or a b) ->
+            And (calculateNNF (Neg a)) (calculateNNF (Neg b))
+
+        And a b ->
+            And (calculateNNF a) (calculateNNF b)
+
+        Or a b ->
+            Or (calculateNNF a) (calculateNNF b)
+
+        a ->
+            a
+
+
+
+-- CNF
+
+
+renderCNF : Formula -> Html Msg
+renderCNF formula =
+    let
+        cnf =
+            calculateCNF formula
+    in
+    div [ class "box content" ]
+        [ h4 [] [ text "CNF" ]
+        , text (toString cnf)
+        ]
+
+
+calculateCNF : Formula -> Formula
+calculateCNF formula =
+    case calculateNNF formula of
+        And a b ->
+            And (calculateCNF a) (calculateCNF b)
+
+        Or a b ->
+            distrCNF (calculateCNF a) (calculateCNF b)
+
+        a ->
+            a
+
+
+distrCNF : Formula -> Formula -> Formula
+distrCNF formula1 formula2 =
+    case ( formula1, formula2 ) of
+        ( And formula11 formula12, _ ) ->
+            And (distrCNF formula11 formula2) (distrCNF formula12 formula2)
+
+        ( _, And formula21 formula22 ) ->
+            And (distrCNF formula1 formula21) (distrCNF formula1 formula22)
+
+        ( a, b ) ->
+            Or a b
+
+
+
+-- DNF
+
+
+renderDNF : Formula -> Html Msg
+renderDNF formula =
+    let
+        dnf =
+            calculateDNF formula
+    in
+    div [ class "box content" ]
+        [ h4 [] [ text "DNF" ]
+        , text (toString dnf)
+        ]
+
+
+calculateDNF : Formula -> Formula
+calculateDNF formula =
+    case calculateNNF formula of
+        Or a b ->
+            Or (calculateDNF a) (calculateDNF b)
+
+        And a b ->
+            distrDNF (calculateDNF a) (calculateDNF b)
+
+        a ->
+            a
+
+
+distrDNF : Formula -> Formula -> Formula
+distrDNF formula1 formula2 =
+    case ( formula1, formula2 ) of
+        ( Or formula11 formula12, _ ) ->
+            Or (distrDNF formula11 formula2) (distrDNF formula12 formula2)
+
+        ( _, Or formula21 formula22 ) ->
+            And (distrDNF formula1 formula21) (distrDNF formula1 formula22)
+
+        ( a, b ) ->
+            And a b
