0ed9d312842b3df1727504926890b0a5bb82057d
Martin Neuner <mfritzn@gmail.com>
2023-02-22 17:50:29 +0100

    Updates

---
+++ precache-manifest.32f966b5a183a100c198e5e392fac9ce.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2351a1da48928196179b2576e2b60fbe",
+    "url": "/my-app/all.js"
+  },
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/my-app/favicon.ico"
+  },
+  {
+    "revision": "98daae11533d17f3c8619b2c82aeaa34",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "2192e9b000f464fc69d31caed0f6ba54",
+    "url": "/my-app/logo.svg"
+  },
+  {
+    "revision": "75b45828c34cdf91aaf25aa8febf87e8",
+    "url": "/my-app/manifest.json"
+  },
+  {
+    "revision": "f9a7baaba8c96490e1a1",
+    "url": "/my-app/static/css/main.056b4050.chunk.css"
+  },
+  {
+    "revision": "b143b2a26b73d4cf2f5c",
+    "url": "/my-app/static/css/vendors~main.34b331dc.chunk.css"
+  },
+  {
+    "revision": "f9a7baaba8c96490e1a1",
+    "url": "/my-app/static/js/main.f9a7baab.chunk.js"
+  },
+  {
+    "revision": "194958df74768bcd08bd",
+    "url": "/my-app/static/js/runtime~main.194958df.js"
+  },
+  {
+    "revision": "b143b2a26b73d4cf2f5c",
+    "url": "/my-app/static/js/vendors~main.b143b2a2.chunk.js"
+  }
+]);
