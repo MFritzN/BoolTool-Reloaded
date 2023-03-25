14a46736d67c91d6fc1975dac3784adef340c7ea
Martin Neuner <mfritzn@gmail.com>
2023-03-25 22:17:21 +0100

    Add truth table sorting

--- src/Representations/TruthTable.elm
+++ src/Representations/TruthTable.elm
@@ -56,7 +56,7 @@ calculateTruthTable formula =
         variables =
             Dict.fromList (List.map (\variable -> ( variable, Basics.False )) (Set.toList (getVariables formula)))
     in
-    { vars = Dict.keys variables, results = ( Dict.values variables, evaluateUnsafe formula variables ) :: calculateTruthTableHelp formula variables }
+    sortTruthTable <| { vars = Dict.keys variables, results = ( Dict.values variables, evaluateUnsafe formula variables ) :: calculateTruthTableHelp formula variables }
 
 
 calculateTruthTableHelp : Formula -> Dict.Dict String Basics.Bool -> List ( List Basics.Bool, Basics.Bool )
@@ -67,3 +67,25 @@ calculateTruthTableHelp formula variables =
 
         Just newVariables ->
             ( Dict.values newVariables, evaluateUnsafe formula newVariables ) :: calculateTruthTableHelp formula newVariables
+
+
+sortTruthTable : TruthTable -> TruthTable
+sortTruthTable truthTable =
+    let
+        bToI b =
+            if b then
+                0
+
+            else
+                1
+    in
+    { truthTable
+        | results =
+            List.sortWith
+                (\p1 p2 ->
+                    case ( p1, p2 ) of
+                        ( ( vs1, _ ), ( vs2, _ ) ) ->
+                            compare (List.map bToI vs1) (List.map bToI vs2)
+                )
+                truthTable.results
+    }
